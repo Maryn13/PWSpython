cd19bd0dd6e090f9873eb3d5a37c42abd1246aa5
Maryn13 <66688025+Maryn13@users.noreply.github.com>
2020-12-07 16:46:38 +0100

    Update Sim_DQN.py

--- Sim_DQN.py
+++ Sim_DQN.py
@@ -3,90 +3,34 @@ import time
 import random
 from tqdm import tqdm
 import os
-
+import matplotlib.pyplot as plt
 import numpy as np
 import nn
+from envrionment import Env
 from class1 import PO as Ballon
 from Target import Target
 import pygame
 
-DISCOUNT = 0.999
-REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
+DISCOUNT = 0.99
+REPLAY_MEMORY_SIZE = 100_000  # How many last steps to keep for model training
 MIN_REPLAY_MEMORY_SIZE = 1_000  # Minimum number of steps in a memory to start training
 MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
-UPDATE_TARGET_EVERY = 100  # Terminal states (end of episodes)
+UPDATE_TARGET_EVERY = 1  # Terminal states (end of episodes)
 MODEL_NAME = '2x256'
 MIN_REWARD = -200  # For model save
 MEMORY_FRACTION = 0.20
 
 # Environment settings
-EPISODES = 20_000
+EPISODES = 2000
 
 # Exploration settings
 epsilon = 1  # not a constant, going to be decayed
-EPSILON_DECAY = 0.99975
+EPSILON_DECAY = 0.99997
 MIN_EPSILON = 0.01
 
 #  Stats settings
-AGGREGATE_STATS_EVERY = 500  # episodes
+AGGREGATE_STATS_EVERY = 10 # episodes
 SHOW_PREVIEW = False
-
-class Env:
-    SIZE = 500
-    RETURN_IMAGES = False
-    MOVE_PENALTY = 1
-    ENEMY_PENALTY = 300
-    FOOD_REWARD = 50
-    #OBSERVATION_SPACE_VALUES =
-    ACTION_SPACE_SIZE = 9
-
-    def reset(self):
-        self.player = Ballon("ballon", 50, 50, 1,1, 100, 1)
-        self.food = Target(100,100,[100,100])
-        self.food.instantiate(x =100, y =100)
-        while self.food.check(self.player.x, self.player.y):
-            self.food.instantiate()
-
-
-        self.episode_step = 0
-
-        if self.RETURN_IMAGES:
-            observation = np.array(self.get_image())
-        else:
-            observation = [self.player.loc[0]-self.food.loc[0], self.player.loc[1]-self.food.loc[1],  self.player.angvel, self.player.angacc]
-
-        return observation
-
-    def step(self, action, dcr):
-        self.episode_step += 1
-        self.player.action(action)
-
-        for i in range(0, int(dcr/self.player.dt)):
-            self.player.setVelocity()
-            self.player.move()
-
-        if self.RETURN_IMAGES:
-            new_observation = np.array(self.get_image())
-        else:
-            new_observation = [self.player.loc[0]-self.food.loc[0], self.player.loc[1]-self.food.loc[1],  self.player.angvel, self.player.angacc]
-
-
-        if self.player == self.food:
-            reward = self.FOOD_REWARD
-        elif self.episode_step >= 500:
-            reward = 25/(np.abs(0.05*self.food.x - 0.05*self.player.loc[0]) + np.abs(0.05*self.food.y - 0.05*self.player.loc[1]))
-        else:
-            reward = -self.MOVE_PENALTY
-
-        done = False
-        if reward == self.FOOD_REWARD or self.episode_step >= 500:
-            done = True
-
-        return new_observation, reward, done
-
-    def render(self, screen):
-        self.player.draw(screen)
-        self.food.draw(screen)
 
 
 
@@ -98,11 +42,10 @@ env = Env()
 ep_rewards = [-500]
 
 # For more repetitive results
-random.seed(1)
-np.random.seed(1)
+
 
 if not os.path.isdir('models'):
-    pass #os.makedirs('models')
+    os.makedirs('models')
 
 #Agent class
 class DQNAgent:
@@ -126,22 +69,25 @@ class DQNAgent:
     def create_model(self):
         model = nn.Model()
 
-        model.add(nn.Layer_Dense(4, 128))
+        model.add(nn.Layer_Dense(6, 64))
         model.add(nn.Activation_ReLU())
 
-        model.add(nn.Layer_Dense(128, 128))
+        model.add(nn.Layer_Dense(64,64))
+        model.add(nn.Activation_ReLU())
+        model.add(nn.Layer_Dense(64, 64))
         model.add(nn.Activation_ReLU())
 
 
-        model.add(nn.Layer_Dense(128, 128))
-        model.add(nn.Activation_ReLU())
 
 
-        model.add(nn.Layer_Dense(128, env.ACTION_SPACE_SIZE))
+
+
+
+        model.add(nn.Layer_Dense(64, env.ACTION_SPACE_SIZE))
         model.add(nn.Activation_Linear())
 
         model.set(loss=nn.Loss_MeanSquaredError(),
-                  optimizer=nn.Optimizer_Adam(learning_rate=0.001, decay=1e-3),
+                  optimizer=nn.Optimizer_Adam(learning_rate=0.001),
                   accuracy=nn.Accuracy_Regression())
         model.finalize()
         return model
@@ -152,7 +98,7 @@ class DQNAgent:
         self.replay_memory.append(transition)
 
     # Trains main network every step during episode
-    def train(self, terminal_state, step):
+    def train(self, terminal_state, step, stats):
 
         # Start training only if certain number of samples is already saved
         if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
@@ -197,24 +143,45 @@ class DQNAgent:
 
         y = np.array(y)
         # Fit on all samples as one batch, log only on terminal state
-        self.model.train(np.array(X), y, epochs=1, batch_size=MINIBATCH_SIZE, show_stats=False)
+        self.model.train(np.array(X), y, epochs=1, batch_size=MINIBATCH_SIZE, show_stats=stats)
 
         # Update target network counter every episode
         if terminal_state:
             self.target_update_counter += 1
 
         # If counter reaches set value, update target network with weights of main network
-        if self.target_update_counter > UPDATE_TARGET_EVERY:
-            self.target_model.set_parameters(self.model.get_parameters())
-            self.target_update_counter = 0
+        self.update_target(True,  0.001)
 
     # Queries main network for Q values given current observation space (environment state)
     def get_qs(self, state):
         return self.model.predict(np.array(state))
 
+    def update_target(self, softupdate, tau):
+        if not softupdate:
+            if self.target_update_counter > UPDATE_TARGET_EVERY:
+                self.target_model.set_parameters(self.model.get_parameters())
+                self.target_update_counter = 0
+            return
+        if softupdate:
+            q_model_theta = self.model.get_parameters()
+            target_model_theta = self.target_model.get_parameters()
+            counter = 0
+            for q_weight, target_weight in zip(q_model_theta, target_model_theta):
+                target_weight = list(target_weight)
+                q_weight = list(q_weight)
+
+                target_weight[0] = target_weight[0] * (1 - tau) + q_weight[0] * tau
+                target_weight[1] = target_weight[1] * (1 - tau) + q_weight[1] * tau
+                target_model_theta[counter] = target_weight
+                counter += 1
+            self.target_model.set_parameters(target_model_theta)
+
 
 agent = DQNAgent()
-
+averages =[]
+episodes = []
+minimums = []
+maximums = []
 # Iterate over episodes
 for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
 
@@ -250,14 +217,14 @@ for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
                     # Get random action
                     action = np.random.randint(0, env.ACTION_SPACE_SIZE)
 
-                new_state, reward, done = env.step(action, agent.inputrate)
+                new_state, reward, done = env.step(action)
 
                 # Transform new continous state to new discrete state and count reward
                 episode_reward += reward
 
                 # Every step we update replay memory and train main network
                 agent.update_replay_memory((current_state, action, reward, new_state, done))
-                agent.train(done, step)
+                agent.train(done, step, False)
 
                 current_state = new_state
                 step += 1
@@ -289,40 +256,55 @@ for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
                 # Get random action
                 action = np.random.randint(0, env.ACTION_SPACE_SIZE)
 
-            new_state, reward, done = env.step(action, agent.inputrate)
+            new_state, reward, done = env.step(action)
 
             # Transform new continous state to new discrete state and count reward
             episode_reward += reward
 
-
-
+            showstats = False
             # Every step we update replay memory and train main network
             agent.update_replay_memory((current_state, action, reward, new_state, done))
-            agent.train(done, step)
+            if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+                showstats = False
+            else:
+                pass
+
+            agent.train(done, step, showstats)
 
             current_state = new_state
             step += 1
+            if epsilon > MIN_EPSILON:
+                epsilon *= EPSILON_DECAY
+                epsilon = max(MIN_EPSILON, epsilon)
 
-    # Append episode reward to a list and log stats (every given number of episodes)
-    ep_rewards.append(episode_reward)
-    if not episode % AGGREGATE_STATS_EVERY or episode == 1:
-        average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
-        print(f'episode{episode}')
-        print(f'average reward: {average_reward}')
-        print(f'reward min: {min_reward}')
-        print(f'max reward: {max_reward}')
-        print(f'epsilon: {epsilon}')
-        # Save model, but only when min reward is greater or equal a set value
-        if min_reward >= MIN_REWARD:
-            agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+        # Append episode reward to a list and log stats (every given number of episodes)
+        ep_rewards.append(episode_reward)
+        if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+            average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:]) / len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            episodes.append(episode)
+            averages.append(average_reward)
+            minimums.append(min_reward)
+            maximums.append(max_reward)
+            print(f'episode{episode}')
+            print(f'average reward: {average_reward}')
+            print(f'reward min: {min_reward}')
+            print(f'max reward: {max_reward}')
+            print(f'epsilon: {epsilon}')
+            print(f'learning rate: {agent.model.optimizer.current_learning_rate}')
+            # Save model, but only when min reward is greater or equal a set value
+            if min_reward >= MIN_REWARD:
+                agent.model.save(
+                    f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
 
-    # Decay epsilon
-    if epsilon > MIN_EPSILON:
-        epsilon *= EPSILON_DECAY
-        epsilon = max(MIN_EPSILON, epsilon)
 
 
 
-    agent.model.save("model1")
+plt.plot(episodes, minimums)
+plt.plot(episodes, averages)
+plt.plot(episodes, maximums)
+plt.show()
+
+
+agent.model.save("model8")
